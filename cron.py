070d31b64da9850b81538aa367b08424f5cd704c
Tayfun Yiğit <75207183+tako007@users.noreply.github.com>
2026-01-29 15:25:47 +0300

    Change date format and remove timezone dependency
    Updated date formatting to include Turkish month names.

--- cron.py
+++ cron.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 from pathlib import Path
-from zoneinfo import ZoneInfo
 
 p = Path("runs.txt")
-now = datetime.now(ZoneInfo("Europe/Istanbul")).isoformat()
+aylar = [
+    "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+    "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"
+]
+now = datetime.now()
+now = f"{now.day} {aylar[now.month - 1]} {now.year} {now.hour:02d}:{now.minute:02d}"
+
 with p.open("a") as f:
     f.write(now + "\n")
 
