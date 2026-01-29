cde4ce09d4118c95643403425ffeb5ae7a052ebe
Tayfun Yiğit <75207183+tako007@users.noreply.github.com>
2026-01-29 15:28:32 +0300

    Update datetime handling for Turkish timezone

--- cron.py
+++ cron.py
@@ -1,15 +1,21 @@
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from pathlib import Path
 
 p = Path("runs.txt")
+
 aylar = [
     "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
     "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"
 ]
-now = datetime.now()
-now = f"{now.day} {aylar[now.month - 1]} {now.year} {now.hour:02d}:{now.minute:02d}"
+
+# UTC+3 tanımı
+tr_tz = timezone(timedelta(hours=3))
+
+now = datetime.now(tr_tz)
+
+formatted = f"{now.day} {aylar[now.month - 1]} {now.year} {now.hour:02d}:{now.minute:02d}"
 
 with p.open("a") as f:
-    f.write(now + "\n")
+    f.write(formatted + "\n")
 
-print("Cron çalıştı:", now)
+print("Cron çalıştı:", formatted)
